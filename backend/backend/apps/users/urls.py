b3b6c369cc8e9c287d119003ddd25ea88e0a6755
chieh1111 <angellin9211@gmail.com>
2025-05-22 20:09:09 +0800

    update profile

--- backend/backend/apps/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from apps.users.views import RegisterView, CustomTokenObtainPairView, LogoutView
-
-urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', CustomTokenObtainPairView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-]

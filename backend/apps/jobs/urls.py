e43cd462dac1e2c3af0e142eabca8055e0916adc
Eric Chen <eric@hahmacbook.local>
2025-05-26 11:57:42 +0800

    後端修改

--- backend/apps/jobs/urls.py
+++ backend/apps/jobs/urls.py
@@ -2,5 +2,5 @@ from rest_framework.routers import DefaultRouter
 from apps.jobs.views import JobOpeningViewSet
 
 router = DefaultRouter()
-router.register(r'', JobOpeningViewSet, basename='job')  # 不加 'jobs'
+router.register(r'jobs', JobOpeningViewSet, basename='job')  # 不加 'jobs'
 urlpatterns = router.urls
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -39,8 +39,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # API URLs
-    path('api/companies/', include('apps.companies.urls')),
-    path('api/jobs/', include('apps.jobs.urls')),
+    # path('api/companies/', include('apps.companies.urls')),
+    path('api/', include('apps.companies.urls')), 
+    # path('api/jobs/', include('apps.jobs.urls')),
+    path('api/', include('apps.jobs.urls')), 
     path('api/users/', include('apps.users.urls')),
 
     # API 文档
@@ -50,3 +52,7 @@ urlpatterns = [
     # Redoc UI:
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# urlpatterns = [
+#     path('api/', include('apps.jobs.urls')),
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
